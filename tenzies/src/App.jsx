b5f0fde422736542e6b81d22cc4160c4293536ce
GabrielJDavila <107373187+GabrielJDavila@users.noreply.github.com>
2023-06-03 22:22:28 -0500

    06/03/23 Updates
    - Confined all user data in a state object, instead of their separate state variables.
    - Added basic functionality to Scoreboard component, able to render user data on screen from local storage. Will switch this to database soon
    - Need to update code format. Specifically, separating certain functions into a "utilities" file for readability.

--- tenzies/src/App.jsx
+++ tenzies/src/App.jsx
@@ -4,37 +4,48 @@ import Scoreboard from './Scoreboard'
 import { nanoid } from 'nanoid'
 import Confetti from 'react-confetti'
 function App() {
-
+  
   const [dice, setDice] = useState(allNewDice())
   const [tenzies, setTenzies] = useState(false)
-  const [rolls, setRolls] = useState(0)
-  const [seconds, setSeconds] = useState(0)
-  const [minutes, setMinutes] = useState(0)
-  const [savedData, setSavedData] = useState([])
+  const [userData, setUserData] = useState({
+    name: "",
+    min: 0,
+    sec: 0,
+    rolls: 0
+  })
+  const [savedData, setSavedData] = useState(
+    JSON.parse(localStorage.getItem("data")) || [])
   const [showScoreboard, setShowScoreboard] = useState(false)
 
   useEffect(() => {
     const interval = setInterval(() => {
       if(!tenzies) {
-        setSeconds(prevSec => prevSec + 1)
-        if(seconds === 59) {
-          setMinutes(prevMin => prevMin + 1)
-          setSeconds(0)
+        setUserData(prevSec => {
+          return {
+            ...prevSec,
+            sec: prevSec.sec + 1
+          }
+        })
+        if(userData.sec === 59) {
+          setUserData(prevMin => {
+            return {
+              ...prevMin,
+              min: prevMin.min + 1
+            }
+          })
+          setUserData(prevSec => {
+            return {
+              ...prevSec,
+              sec: 0
+            }
+          })
         }
       }
     }, 1000)
     return () => {
       clearInterval(interval)
     }
-  }, [seconds])
-
-  useEffect(() => {
-    const data = localStorage.getItem("data")
-    if(data) {
-      setSavedData(data)
-      console.log(data.score)
-    }
-  }, [])
+  }, [userData])
 
   useEffect(() => {
     const allHeld = dice.every(die => die.isHeld)
@@ -67,13 +78,24 @@ function App() {
           die :
           generateNewDie()
       }))
-      setRolls(oldRolls => oldRolls + 1)
+      setUserData(oldData => {
+        return {
+          ...oldData,
+          rolls: oldData.rolls + 1
+        }
+      })
     } else {
       setTenzies(false)
       setDice(allNewDice())
-      setRolls(0)
-      setSeconds(0)
-      setMinutes(0)
+      setUserData(prevData => {
+        return {
+          ...prevData,
+          name: "",
+          min: 0,
+          sec: 0,
+          rolls: 0
+        }
+      })
     }
   }
 
@@ -104,14 +126,22 @@ function App() {
   }
 
   function addToScoreboard() {
-    const score = {
-      timeMin: minutes,
-      timeSec: seconds,
-      rollCount: rolls
-    }
-    localStorage.setItem("data", score)
+    let data = userData
+    const updatedData = [...savedData, data]
+    localStorage.setItem("data", JSON.stringify(updatedData))
+    setSavedData(updatedData)
+    console.log(savedData)
   }
 
+  function setUser(e) {
+    const {name, value} = e.target
+    setUserData(prevData => {
+      return {
+        ...prevData,
+        [name]: value
+      }
+    })
+  }
   return (
     <div>
       {
@@ -121,8 +151,8 @@ function App() {
         <h1 className="title">Tenzies</h1>
         <p className="instructions">{tenzies ? "You Won!" : "Roll until all dice are the same. Click each die to freeze it at its current value between rolls."}</p>
         <div className="rolls-timer-container">
-          <p className="rolls">Rolls: {rolls}</p>
-          <p className="timer">Time: {minutes < 10 && 0}{minutes}:{seconds < 10 && 0}{seconds}</p>
+          <p className="rolls">Rolls: {userData.rolls}</p>
+          <p className="timer">Time: {userData.min < 10 && 0}{userData.min}:{userData.sec < 10 && 0}{userData.sec}</p>
           <button className="scoreboard-btn" onClick={flipShow}>Scoreboard</button>
         </div>
         <div className="main-div">
@@ -130,7 +160,24 @@ function App() {
         </div>
         <div className="bottom-btns-container">
           <button onClick={rollNewDice} className="roll-dice">{tenzies ? "New Game" : "Roll New Dice"}</button>
-          {tenzies && <button onClick={addToScoreboard}>Add to scoreboard</button>}
+          {tenzies &&
+          <div className="user-info">
+            <input
+              type="text"
+              className="username"
+              placeholder="enter name"
+              name="name"
+              value={userData.name}
+              onChange={setUser}
+            >
+            </input>
+            <button
+              onClick={addToScoreboard}
+            >
+              Add to scoreboard
+            </button>
+          </div>
+          }
         </div>
       </main>
       }
@@ -138,6 +185,7 @@ function App() {
         showScoreboard &&
         <Scoreboard
           flipShow={flipShow}
+          data={savedData}
         />
         }
     </div>
--- tenzies/src/Scoreboard.jsx
+++ tenzies/src/Scoreboard.jsx
@@ -1,13 +1,28 @@
 
 export default function Scoreboard(props) {
+    
+    const users = props.data.map(user => <p>{user.name}</p>)
+    const userTime = props.data.map(user => <p>{user.min} : {user.sec}</p>)
+    const userRolls = props.data.map(user => <p>{user.rolls}</p>)
+    
     return (
         <div>
             <h1 className="scoreboard-title">Scoreboard</h1>
             <div className="scoreboard-container">
-                <p>name</p>
-                <p>time</p>
-                <p>rolls</p>
-                <p>spot</p>
+                <div className="names-column">
+                    <p>Name</p>
+                    {users}
+                </div>
+                <div className="time-column">
+                    <p>Time</p>
+                    {userTime}
+                </div>
+                <div className="rolls-column">
+                    <p>Rolls</p>
+                    {userRolls}
+                </div>
+                <div className="spot-column"></div>
+                
             </div>
             <button className="backToGame" onClick={props.flipShow}>back to game</button>
         </div>
